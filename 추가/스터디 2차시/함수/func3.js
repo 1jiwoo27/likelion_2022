f152afb01d79c7bc05c53b1a3184127f752de488
1jiwoo27 <chloey1227@naver.com>
2022-04-27 23:02:10 +0900

    javascript

---
+++ 추가/스터디 2차시/함수/func3.js
@@ -0,0 +1,44 @@
+function printBanana(){
+    console.log("banana");
+}
+printBanana();
+
+function printApple(){
+    console.log("apple");
+}
+printApple();
+
+function printOrange(){
+    console.log("orange");
+}
+printOrange();
+
+function printMelon(){
+    console.log("melon");
+}
+printMelon();
+
+function printGrape(){
+    console.log("grape");
+}
+printGrape();
+
+function printStrawberry(){
+    console.log("strawberry");
+}
+printStrawberry();
+
+function printMango(){
+    console.log("mango");
+}
+printMango();
+
+function printGrape(){
+    console.log("grape");
+}
+printGrape();
+
+function printLemon(){
+    console.log("lemon");
+}
+printLemon();
